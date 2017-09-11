0ffe076f9248d19da24d4b84ae1dc9a1ed6f3964
Gergo Nagy <guzmop@gmail.com>
2017-09-11 08:19:39 +0100

    css

--- public/showData.js
+++ public/showData.js
@@ -1,6 +1,7 @@
 var ShowsDatas = function (url) {
     this.url = url;
-    this.data = []
+    this.tvShows = []
+    this.episodes = []
 }
 
 ShowsDatas.prototype.getData = function (search) {
@@ -10,16 +11,16 @@ ShowsDatas.prototype.getData = function (search) {
         if (xhr.status !== 200) return;
 
         var jsonString = xhr.responseText;
-         this.data = JSON.parse(jsonString);
+        this.tvShows = JSON.parse(jsonString);
 
         var div = document.querySelector('#shows');
         div.innerHTML = "";
-        
-        for (var i = 0; i < this.data.length; i++) {
-            
+
+        for (var i = 0; i < this.tvShows.length; i++) {
+
             var output = document.createElement('div');
-            var element = this.data[i];
-            
+            var element = this.tvShows[i];
+
             output.innerHTML += `
                     <div class="col-md-3">
                         <div class="well text-center">
@@ -32,19 +33,19 @@ ShowsDatas.prototype.getData = function (search) {
             div.appendChild(output)
             console.log(element.show.id)
         }
-    
+
     }.bind(this));
 
     xhr.send();
 }
 
-ShowsDatas.prototype.tvShowSelected = function(id){
+ShowsDatas.prototype.tvShowSelected = function (id) {
     sessionStorage.setItem('showId', id)
     window.location = 'tvshow.html';
     return false;
 }
 
-ShowsDatas.prototype.getTvShow = function(){
+ShowsDatas.prototype.getTvShow = function () {
     var tvShowId = sessionStorage.getItem('showId');
 
     var xhr = new XMLHttpRequest();
@@ -53,42 +54,103 @@ ShowsDatas.prototype.getTvShow = function(){
         if (xhr.status !== 200) return;
 
         var jsonString = xhr.responseText;
-        var data = JSON.parse(jsonString);
-        
-        console.log(data)
+        var tvShow = JSON.parse(jsonString);
+
+        console.log(tvShow)
         var div = document.querySelector('#show')
 
         var output = document.createElement('div')
         output.innerHTML = `
         <div class="row">
           <div class="col-md-4">
-            <img src="${data.image.original}" class="thumbnail">
+            <img src="${tvShow.image.original}" class="thumbnail">
           </div>
           <div class="col-md-8">
-            <h2>${data.name}</h2>
+            <h2>${tvShow.name}</h2>
             <ul class="list-group">
-              <li class="list-group-item"><strong>Genre:</strong> ${data.genres}</li>
-              <li class="list-group-item"><strong>Released:</strong> ${data.premiered}</li>
-              <li class="list-group-item"><strong>Status:</strong> ${data.status}</li>
-              <li class="list-group-item"><strong>Running:</strong> ${data.schedule.days} at ${data.schedule.time} </li>
-              <li class="list-group-item"><strong>IMDB Rating:</strong> ${data.rating.average}</li>
-              <li class="list-group-item"><strong>Language:</strong> ${data.language}</li>
-              <li class="list-group-item"><strong>Official Site:</strong> ${data.officialSite}</li>
+              <li class="list-group-item"><strong>Genre:</strong> ${tvShow.genres}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${tvShow.premiered}</li>
+              <li class="list-group-item"><strong>Status:</strong> ${tvShow.status}</li>
+              <li class="list-group-item"><strong>Running:</strong> ${tvShow.schedule.days} at ${tvShow.schedule.time} </li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${tvShow.rating.average}</li>
+              <li class="list-group-item"><strong>Language:</strong> ${tvShow.language}</li>
+              <li class="list-group-item"><strong>Official Site:</strong> ${tvShow.officialSite}</li>
+              <li class="list-group-item"><strong>Official Site:</strong> ${this.episodes}</li>
             </ul>
           </div>
         </div>
         <div class="row">
           <div class="well">
             <h3>Plot</h3>
-            ${data.summary}
+            ${tvShow.summary}
             <hr>
-            <a href="http://imdb.com/title/${data.externals.imdb}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="http://imdb.com/title/${tvShow.externals.imdb}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a onclick="ShowsDatas.prototype.seasonsSelected('${tvShow.id}')" class="btn btn-default href="#">Seasons</a>
             <a href="/" class="btn btn-default">Go Back To Search</a>
-          </div>
-        </div>
       `;
 
         div.appendChild(output)
+
+    }.bind(this));
+
+    xhr.send();
+
+}
+
+ShowsDatas.prototype.seasonsSelected = function (id) {
+    sessionStorage.setItem('showId', id)
+    window.location = 'seasons.html';
+    return false;
+}
+
+ShowsDatas.prototype.getSeasons = function () {
+    var tvShowId = sessionStorage.getItem('showId');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('Get', 'http://api.tvmaze.com/shows/' + tvShowId + '/episodes');
+    xhr.addEventListener('load', function () {
+        if (xhr.status !== 200) return;
+
+        var jsonString = xhr.responseText;
+        this.episodes = JSON.parse(jsonString);
+
+
+
+        for (var i = 0; i < this.episodes.length; i++) {
+            var episode = this.episodes[i];
+            console.log(episode)
+
+            var div = document.querySelector('#seasons')
+
+            var output = document.createElement('div')
+            output.innerHTML = `
+                    <div class="row">
+                    <div class="col-md-4">
+                        <img src="${episode.image.original}" class="thumbnail">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>${episode.name} </h3>
+                        <h5>${episode.airdate} </h5>
+                        <ul class="list-group">
+                        <li class="list-group-item"><strong>Season:</strong> ${episode.season}</li>
+                        <li class="list-group-item"><strong>Episode:</strong> ${episode.number}</li>
+                        <li class="list-group-item"><strong>Runtime:</strong> ${episode.runtime}</li>
+                        <li class="list-group-item"><strong>Airdate:</strong> ${episode.airdate}</li>
+                    </div>
+                    </div>
+                    <div class="row">
+                    <div class="well">
+                        <h3>Plot</h3>
+                        ${episode.summary}
+                        <hr>
+                        <a href="/" class="btn btn-default">Go Back To Search</a>
+                `;
+
+            div.appendChild(output)
+
+
+        }
+
 
     }.bind(this));
 
